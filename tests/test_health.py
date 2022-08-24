ed7e34d02068e930cfe228bbb5a3773f49e4c784
Erlend Skaldehaug Riis <kons-riis@ssb.no>
2022-08-24 11:07:08 +0200

    fix testing errors to run tests locally
    - changed test client framework from fastapi to flask
    - changed test_ready bug from json response to text response

--- tests/test_health.py
+++ tests/test_health.py
@@ -1,17 +1,29 @@
 import logging
-from fastapi.testclient import TestClient
-import app as api
-
-client = TestClient(api)
+import pytest
+from google.cloud import bigquery
 
 logger = logging.getLogger()
 
+# @pytest.fixture(scope='function')
+# def mock_bigquery_client(monkeypatch):
+#     def mockclient(*args, **kwargs):
+#         return None
+#     monkeypatch.setattr(bigquery,"Client",mockclient)
+
+# @pytest.fixture(scope='function')
+# def client(mock_bigquery_client):
+#     from app.app import app as api
+#     client = api.test_client()
+#     return client
+
+from app.app import app as api
+client = api.test_client()
 
 def test_ready():
     """Tests the ready endpoint. Is always 200 for now"""
     response = client.get("/health/ready")
     assert response.status_code == 200
-    assert response.json() == "Ready!"
+    assert response.text == "Ready!"
 
 
 def test_alive():

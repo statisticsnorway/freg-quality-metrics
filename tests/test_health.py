d96425f2c5fc0344dc4c342be3d8a35928986f6c
Erlend Skaldehaug Riis <kons-riis@ssb.no>
2022-08-25 16:14:45 +0200

    Add mock bigquery client to tests

--- tests/test_health.py
+++ tests/test_health.py
@@ -1,33 +1,35 @@
 import logging
 import pytest
-from google.cloud import bigquery
+from unittest.mock import patch, MagicMock
 
-logger = logging.getLogger()
+logger = logging.getLogger(__name__)
 
-# @pytest.fixture(scope='function')
-# def mock_bigquery_client(monkeypatch):
-#     def mockclient(*args, **kwargs):
-#         return None
-#     monkeypatch.setattr(bigquery,"Client",mockclient)
+@pytest.fixture(scope='session')
+def mock_bigquery_client():
+    mock_client = MagicMock()
+    with patch('google.cloud.bigquery.Client', autospec=False):
+        yield mock_client
 
-# @pytest.fixture(scope='function')
-# def client(mock_bigquery_client):
-#     from app.app import app as api
-#     client = api.test_client()
-#     return client
+@pytest.fixture(scope='session')
+def client(mock_bigquery_client):
+    from freg_quality_metrics.app import app as api
+    client = api.test_client()
+    return client
 
-from app.app import app as api
-client = api.test_client()
+# from api import bigquery
 
-def test_ready():
+
+# from app.app import app as api
+# client = api.test_client()
+
+def test_ready(client):
     """Tests the ready endpoint. Is always 200 for now"""
     response = client.get("/health/ready")
     assert response.status_code == 200
     assert response.text == "Ready!"
 
 
-def test_alive():
+def test_alive(client):
     """Tests the alive endpoint. Is always 200 for now"""
     response = client.get("/health/alive")
-
     assert response.status_code == 200
